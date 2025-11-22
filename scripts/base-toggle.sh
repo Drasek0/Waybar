02d4ea90fd8819093301c6395bb8cb93425b266e
Drasek <drasek0@proton.me>
2025-11-22 02:23:21 +0100

    Add more toggle files -base-toggle.sh -laptop-toggle.sh

---
+++ scripts/base-toggle.sh
@@ -0,0 +1,1 @@
+pkill waybar || waybar -c ~/git/Waybar/base/base-config -s ~/git/Waybar/base/base-style.css
---
+++ scripts/laptop-toggle.sh
@@ -0,0 +1,1 @@
+pkill waybar || waybar -c ~/git/Waybar/laptop/laptop-config -s ~/git/Waybar/laptop/laptop-style.css
